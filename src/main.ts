bcfaba0059fa9d1d154d58b94a6d67480830adde
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-01-14 07:13:46 +0900

    🚧 WIP : first-steps
    - https://docs.nestjs.com/first-steps

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express/interfaces';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    abortOnError: false,
+  });
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
