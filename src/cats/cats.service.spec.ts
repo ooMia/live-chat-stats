389b538f8dd610bc048894128a16f9693a6d4dc1
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-01-15 07:47:12 +0900

    Feature/providers (#2)
    - https://docs.nestjs.com/providers

--- src/cats/cats.service.spec.ts
+++ src/cats/cats.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 describe('CatsService', () => {
   let service: CatsService;
@@ -13,14 +14,23 @@ describe('CatsService', () => {
   });
 
   it('should be defined', async () => {
-    const catObj = { name: 'test', age: 1, breed: 'test' };
-    expect(service).toBeDefined();
-    expect(service.findAll()).toEqual('This action returns all cats');
-    expect(service.findOne(1)).toEqual('This action returns a #1 cat');
-    expect(service.remove(1)).toEqual('This action removes a #1 cat');
-    expect(service.create(catObj)).toEqual(
+    const catCreateDto = new CreateCatDto(
+      'test',
+      new Date().getFullYear() - 1 + '-01-01',
+      'test',
+    );
+    expect(service.create(catCreateDto)).toEqual(
       'This action adds a new cat with the following details: {"name":"test","age":1,"breed":"test"}',
     );
+
+    const catObj = { name: 'test', age: 1, breed: 'test' };
+    expect(service).toBeDefined();
+    expect(service.findAll()).toBeInstanceOf(Array);
+    expect(service.findAll().length).toBeGreaterThanOrEqual(0);
+
+    expect(service.findOne(1)).toEqual('This action returns a #1 cat');
+    expect(service.remove(1)).toEqual('This action removes a #1 cat');
+
     expect(service.update(1, catObj)).toEqual(
       'This action updates a #1 cat with the following details: {"name":"test","age":1,"breed":"test"}',
     );
