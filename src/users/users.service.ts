1bac5367e6ba2b21501ee0ce92db0ae84cc3179f
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-01-16 16:26:05 +0900

    🚧 WIP : database / repository-pattern

---
+++ src/users/users.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    const user = new User();
+    user.firstName = createUserDto.firstName;
+    user.lastName = createUserDto.lastName;
+
+    return this.usersRepository.save(user);
+  }
+
+  async findAll(): Promise<User[]> {
+    Logger.log('Fetching all users: controller');
+    return this.usersRepository.find();
+  }
+
+  findOne(id: number): Promise<User> {
+    Logger.log('Fetching one user: controller');
+    return this.usersRepository.findOneBy({ id: id });
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.usersRepository.delete(id);
+  }
+}
