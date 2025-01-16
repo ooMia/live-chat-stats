6f66843515d91feeac83193519f84c108ddc4d95
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-01-16 16:31:23 +0900

    🐛 Fix : module import in order
    - CatsModule이 Express의 객체를 직접 사용하여 다른 모듈의 동작을 방해하는 문제 해결
    - CatsModule을 마지막에 로드
    - 모듈 간의 라이브러리 의존성 문제 해결

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,8 +9,23 @@ import { UserHttpModule } from './users/users-http.module';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-  // https://docs.nestjs.com/controllers#getting-up-and-running
-  imports: [CatsModule],
+  imports: [
+    UserHttpModule,
+    // https://docs.nestjs.com/techniques/database
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'test',
+      password: 'test',
+      database: 'test',
+      entities: [User],
+      synchronize: true, // shouldn't be used in production - otherwise you can lose production data.
+      autoLoadEntities: true,
+      // etc... https://typeorm.io/data-source-options/
+    }),
+    CatsModule, // module which use library-specific approach should be imported last
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
