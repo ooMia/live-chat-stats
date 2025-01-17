c265cb54cd28fe1a9f02993526176465f2bf83ea
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-01-17 09:12:21 +0900

    ✨ Feat : create resource photos

--- src/users/users-http.module.ts
+++ src/users/users-http.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersModule } from './users.module';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+
+import { PhotosModule } from './photos.module';
+import { PhotosService } from './photos.service';
 
 @Module({
-  imports: [UsersModule],
-  // inject the UsersRepository into the UsersService using the @InjectRepository() decorator
-  providers: [UsersService],
+  imports: [UsersModule, PhotosModule],
+  // inject Repository into Service using the @InjectRepository() decorator
+  providers: [UsersService, PhotosService],
   controllers: [UsersController],
 })
 export class UserHttpModule {}
