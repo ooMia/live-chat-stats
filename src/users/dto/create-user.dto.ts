4febfce9a8678eab7557c655d6b8df97a46da297
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-02-02 20:05:15 +0900

    Hotfix/dockerize (#7)
    * 🐛 Fix : DB conn error on docker prod env

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,10 @@
+import { IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
   firstName: string;
+  @IsString()
   lastName: string;
+  @IsString({ each: true })
   profileUrls?: string[];
 }
